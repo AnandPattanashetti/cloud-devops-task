91162fcbc4f388a6cbeccefbdd3ec85a2137f1ee
Anand Pattanashetti <134671487+AnandPattanashetti@users.noreply.github.com>
2024-12-01 12:09:36 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,13 +1,14 @@
-variable "kubernetes_version" {
-  default     = 1.27
-  description = "kubernetes version"
+variable "atc_cloud_task_kubernetes_version" {
+  default     = "1.27"
+  description = "Kubernetes version"
 }
 
-variable "vpc_cidr" {
+variable "atc_cloud_task_vpc_cidr" {
   default     = "10.0.0.0/16"
-  description = "default CIDR range of the VPC"
+  description = "Default CIDR range of the VPC"
 }
-variable "aws_region" {
-  default = "ap-south-1"
-  description = "aws region"
+
+variable "atc_cloud_task_aws_region" {
+  default     = "ap-south-1"
+  description = "AWS region"
 }
